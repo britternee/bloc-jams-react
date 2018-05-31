5bf9dc0c0ac0f1173105bcae97250a1904bcef48
Brittany Miller <britmill@gmail.com>
2018-05-30 18:39:47 -0600

    Checkpoint 1759 Audio Playback done but no buttons?

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,9 +10,40 @@ class Album extends Component {
         });
 
         this.state = {
-            album: album
+            album: album,
+            currentSong: album.songs[0],
+            isPlaying: false
         };
+
+        this.audioElement = document.createElement('audio');
+        this.audioElement.src = album.songs[0].audioSrc;
     }
+
+        play() {
+            this.audioElement.play();
+            this.setState({ isPlaying: true });
+        }
+
+        pause() {
+            this.audioElement.pause();
+            this.setState({ isPlaying: false });
+        }
+
+        setSong(song) {
+            this.audioElement.src = song.audioSrc;
+            this.setState({ currentSong: song });
+        }
+
+        handleSongClick(song) {
+            const isSameSong = this.state.currentSong === song;
+            if (this.state.isPlaying && isSameSong) {
+                this.pause();
+            } else {
+                if (!isSameSong) { this.setSong(song); }
+                this.play();
+            }
+        }
+    
 
     render () {
         return (
@@ -39,9 +70,8 @@ class Album extends Component {
                         <col id="song-duration-column" />
                     </colgroup>
                     <tbody>
-                        { 
-                            this.state.album.songs.map( (song, index ) =>
-                        <tr key={index}>
+                        {this.state.album.songs.map( (song, index ) =>
+                        <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                             <td id="song-number">{index + 1}</td>
                             <td id="song-title">{song.title}</td>
                             <td id="song-duration">{song.duration}</td>
